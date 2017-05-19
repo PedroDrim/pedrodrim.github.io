96f8dd4749cf90b06dafba23efe410f61c649f4f
pedrodrim <pedrohenriquedrim@gmail.com>
2017-05-19 16:13:45 -0300

    implementação de um template html por meio do underlineJs

--- user/mountItens.js
+++ user/mountItens.js
@@ -1,5 +1,64 @@
 // flag -> Projetos ou Experimentos
 
+var templateCard = _.template(
+    '<div class="col-sm-4 portfolio-item">' +
+        '<div class="portfolio-link" href="#<%= i.id %>" data-toggle="modal">' +
+            '<div class="caption">' +
+                '<div class="caption-content">' +
+                    '<i class="fa fa-search-plus fa-3x"></i>' +
+                '</div>' +
+            '</div>' +
+            '<img class="img-fluid" src="user/projetos/<%= i.imagem %>" alt="">' +
+        '</div>' +
+    '</div>'
+);
+
+var templateConteudo = _.template(
+    '<div class="portfolio-modal modal fade" id="<%= i.id %>" tabindex="-1" role="dialog" aria-hidden="true">' +
+        '<div class="modal-dialog" role="document">' +
+            '<div class="modal-content">' +
+                '<div class="close-modal" data-dismiss="modal">' +
+                    '<div class="lr">' +
+                    '<div class="rl">' +
+                    '</div>' +
+                    '</div>' +
+                '</div>' +
+                '<div class="container">' +
+                    '<div class="row">' +
+                    '<div class="col-lg-8 offset-lg-2">' +
+                        '<div class="modal-body">' +
+                            '<h2><%= i.titulo %></h2>' +
+                            '<hr class="star-primary">' +
+                            '<img class="img-fluid img-centered" src="user/projetos/<%= i.imagem %>" alt="">' +
+                            '<p><%= i.descricao %></p>' +
+                            '<ul class="list-inline item-details">' +
+                                '<li>Cliente:' +
+                                '<strong><a href="<%= i.cliente.url %>"> <%= i.cliente.nome %></a>' +
+                                '</strong>' +
+                                '</li>' +
+                                '<li>Data:' +
+                                '<strong><%= i.data %>' +
+                                '</strong>' +
+                                '</li>' +
+                                '<li>Tipo de serviço:' +
+                                '<strong><%= i.servico %>' +
+                                '</strong>' +
+                                '</li>' +
+                                '<li>Versão:' +
+                                '<strong><a href="<%= i.versao.url %>"><%= i.versao.nome %></a>' +
+                                '</strong>' +
+                                '</li>' +
+                            '</ul>' +
+                            '<button class="btn btn-success" type="button" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>' +
+                        '</div>' +
+                    '</div>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+        '</div>' +
+    '</div>'
+);
+
 var mountItem = function(objProject, flag){
     
     var indexCard = "#card" + flag + " .row";
@@ -7,66 +66,12 @@ var mountItem = function(objProject, flag){
     
     //=======[card]===================//
     var card = $(indexCard);
-           
-    var html = "<div class=\"col-sm-4 portfolio-item\">"
-    html += "<div class=\"portfolio-link\" href=\"#portfolioModal2\" data-toggle=\"modal\">"
-    html += "<div class=\"caption\">"
-    html += "<div class=\"caption-content\">"
-    html += "<i class=\"fa fa-search-plus fa-3x\"></i>"
-    html += "</div>"
-    html += "</div>"
-    html += "<img class=\"img-fluid\" src=\"user/projetos/img/cake.png\" alt=\"\">"
-    html += "</div>"
-    html += "</div>"
-
-    console.log(card);
-    card.append(html);
+    card.html( templateCard({i: objProject}) );
     //=======[card]===================//
 
     //=======[content]================//
     var conteudoCard = $(indexConteudo);
-    
-    var html = "<div class=\"portfolio-modal modal fade\" id=\"portfolioModal2\" tabindex=\"-1\" role=\"dialog\" aria-hidden=\"true\">"
-    html += "<div class=\"modal-dialog\" role=\"document\">"
-    html += "<div class=\"modal-content\">"
-    html += "<div class=\"close-modal\" data-dismiss=\"modal\">"
-    html += "<div class=\"lr\">"
-    html += "<div class=\"rl\">"
-    html += "</div>"
-    html += "</div>"
-    html += "</div>"
-    html += "<div class=\"container\">"
-    html += "<div class=\"row\">"
-    html += "<div class=\"col-lg-8 offset-lg-2\">"
-    html += "<div class=\"modal-body\">"
-    html += "<h2>Project Title</h2>"
-    html += "<hr class=\"star-primary\">"
-    html += "<img class=\"img-fluid img-centered\" src=\"user/projetos/img/cake.png\" alt=\"\">"
-    html += "<p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href=\"https://sellfy.com/p/8Q9P/jV3VZ/\">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>"
-    html += "<ul class=\"list-inline item-details\">"
-    html += "<li>Client:"
-    html += "<strong><a href=\"http://startbootstrap.com\">Start Bootstrap</a>"
-    html += "</strong>"
-    html += "</li>"
-    html += "<li>Date:"
-    html += "<strong><a href=\"http://startbootstrap.com\">April 2014</a>"
-    html += "</strong>"
-    html += "</li>"
-    html += "<li>Service:"
-    html += "<strong><a href=\"http://startbootstrap.com\">Web Development</a>"
-    html += "</strong>"
-    html += "</li>"
-    html += "</ul>"
-    html += "<button class=\"btn btn-success\" type=\"button\" data-dismiss=\"modal\"><i class=\"fa fa-times\"></i> Close</button>"
-    html += "</div>"
-    html += "</div>"
-    html += "</div>"
-    html += "</div>"
-    html += "</div>"
-    html += "</div>"
-    html += "</div>"
-
-    conteudoCard.append(html)
+    conteudoCard.html( templateConteudo({i: objProject}))
 //=======[content]================//
 }
 
